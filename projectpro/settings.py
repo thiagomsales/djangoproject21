a186e9967eae6b1d4c4e514912bcfa0dc08c90e2
thiagomsales <tmsales26@gmail.com>
2021-02-23 21:13:37 -0300

    restrição de hosts

--- projectpro/settings.py
+++ projectpro/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't57gozo-e!4cmn^o#7jxjaky-wz#hfd9m(+hy%on91=3rc-e=$'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv()) #['projectpro21.herokuapp.com']
 
 
 # Application definition
