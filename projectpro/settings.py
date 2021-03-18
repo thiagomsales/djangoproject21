a942c9f9f3bebf462b20d759655d8dd8774334dc
thiagomsales <tmsales26@gmail.com>
2021-03-18 16:43:18 -0300

    collect fast false

--- projectpro/settings.py
+++ projectpro/settings.py
@@ -136,7 +136,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
-COLLECTFAST_ENABLE = True
+COLLECTFAST_ENABLE = False
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
