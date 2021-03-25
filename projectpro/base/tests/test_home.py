9807dcd16c7dc6b8bf40ef5a5f826b8c8d4fcce8
thiagomsales <tmsales26@gmail.com>
2021-03-25 12:22:30 -0300

    test conteudo

--- projectpro/base/tests/test_home.py
+++ projectpro/base/tests/test_home.py
@@ -1,6 +1,13 @@
 from django.test import Client
 
+from projectpro.django_assertions import assert_contains
 
-def test_status_code(client:Client):
+
+def test_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
+
+
+def test_title(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<title>ProjectPro 21</title>')
