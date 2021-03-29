6e12b61bbbc35eecd44ea02736aed1de61221704
thiagomsales <tmsales26@gmail.com>
2021-03-29 16:35:22 -0300

    Correcao home

--- projectpro/urls.py
+++ projectpro/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', include('projectpro.base.urls')),
+    path('', include('projectpro.base.urls')),
 
 ]
 
